61e059ac3d6a2469aeea6830a5750ed80127a804
timothyhartanto <hartantotimothy@gmail.com>
2015-11-25 23:04:49 +0700

    Update videoPlay.java

--- videoPlay.java
+++ videoPlay.java
@@ -3,21 +3,26 @@ package com.example.proto.videoplay;
 import android.app.Activity;
 import android.net.Uri;
 import android.os.Bundle;
+import android.widget.MediaController;
 import android.widget.VideoView;
 
 public class videoPlay extends Activity {
 
     VideoView vid;
-    
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_video_play);
-        
+
         vid = (VideoView)findViewById(R.id.video1);
-        
+
         String videoPath = "android.resource://" + getPackageName() + "/" + R.raw.videoex;
         vid.setVideoURI(Uri.parse(videoPath));
         vid.start();
+
+        MediaController mc = new MediaController(this);
+        mc.setMediaPlayer(vid);
+        vid.setMediaController(mc);
     }
 }
